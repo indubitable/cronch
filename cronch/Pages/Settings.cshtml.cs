fdc84ff9465df15c1c93d343f488bd84360de1c3
Sasha Kotlyar <arktronic@users.noreply.github.com>
2023-10-08 23:25:43 -0400

    Initial code commit

---
+++ cronch/Pages/Settings.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cronch.Pages
+{
+    public class SettingsModel : PageModel
+    {
+        private readonly ILogger<SettingsModel> _logger;
+
+        public SettingsModel(ILogger<SettingsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
