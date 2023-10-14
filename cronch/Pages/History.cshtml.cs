a3519047110976fe7400ba61d8706b061bdd5e9c
Sasha Kotlyar <arktronic@users.noreply.github.com>
2023-10-14 14:00:43 -0400

    Some basic site design

---
+++ cronch/Pages/History.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cronch.Pages
+{
+    public class HistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ cronch/Pages/Manage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cronch.Pages
+{
+    public class ManageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
