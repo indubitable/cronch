d69789dd56a4737c88129e100919d7f3e3d0d227
Sasha Kotlyar <arktronic@users.noreply.github.com>
2023-10-29 19:28:51 -0400

    Show enabled/total jobs in overview; fix job save button style

--- cronch/Pages/Index.cshtml.cs
+++ cronch/Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using cronch.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace cronch.Pages
+namespace cronch.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+    private readonly JobConfigService _jobConfigService;
+
+    public int EnabledJobCount { get; set; }
+    public int TotalJobCount { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger, JobConfigService jobConfigService)
     {
-        private readonly ILogger<IndexModel> _logger;
+        _logger = logger;
+        _jobConfigService = jobConfigService;
+    }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
-        }
+    public void OnGet()
+    {
+        var allJobs = _jobConfigService.GetAllJobs();
+        EnabledJobCount = allJobs.Where(j => j.Enabled).Count();
+        TotalJobCount = allJobs.Count;
     }
 }
