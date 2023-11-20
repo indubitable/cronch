ff86f6dbb4c45431ec20514b32bb2b2ed3de2c3f
Sasha Kotlyar <arktronic@users.noreply.github.com>
2023-11-19 23:47:13 -0500

    Working on execution details page

--- cronch/Utility.cs
+++ cronch/Utility.cs
@@ -1,5 +1,57 @@
-﻿namespace cronch;
+﻿using cronch.Models;
+
+namespace cronch;
 
 public static class Utility
 {
+    public static string ToUserString(this ExecutionReason reason)
+    {
+        return reason switch
+        {
+            ExecutionReason.Scheduled => "Scheduled",
+            ExecutionReason.Manual => "Manual",
+            _ => "Unknown",
+        };
+    }
+
+    public static string ToUserString(this ExecutionReason? reason)
+    {
+        return (reason.HasValue ? reason.Value.ToUserString() : "Unknown");
+    }
+
+    public static string ToUserString(this TerminationReason reason)
+    {
+        return reason switch
+        {
+            TerminationReason.NoneSpecified => "Not specified",
+            TerminationReason.Exited => "Exited normally",
+            TerminationReason.TimedOut => "Timed out",
+            TerminationReason.SkippedForParallelism => "Skipped due to parallelism limit",
+            _ => "Unknown",
+        };
+    }
+
+    public static string ToUserString(this TerminationReason? reason)
+    {
+        return (reason.HasValue ? reason.Value.ToUserString() : "Unknown");
+    }
+
+    public static string ToUserString(this ExecutionStatus status)
+    {
+        return status switch
+        {
+            ExecutionStatus.Unknown => "Unknown",
+            ExecutionStatus.Running => "Running",
+            ExecutionStatus.CompletedAsSuccess => "Completed successfully",
+            ExecutionStatus.CompletedAsIndeterminate => "Completed as indeterminate",
+            ExecutionStatus.CompletedAsWarning => "Completed with warning",
+            ExecutionStatus.CompletedAsError => "Completed with error",
+            _ => "Unknown",
+        };
+    }
+
+    public static string ToUserString(this ExecutionStatus? status)
+    {
+        return (status.HasValue ? status.Value.ToUserString() : "Unknown");
+    }
 }
