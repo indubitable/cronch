6662efe77b6b8a887c69c53b7e8aa8d453ac3661
Sasha Kotlyar <arktronic@users.noreply.github.com>
2023-12-01 22:39:21 -0500

    Add htmx to execution details, with smooth scrolling of output

--- cronch/Pages/ExecutionDetails.cshtml.cs
+++ cronch/Pages/ExecutionDetails.cshtml.cs
@@ -1,6 +1,7 @@
 using cronch.Models.ViewModels;
 using cronch.Services;
 using cronch.Utilities;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Text;
 using System.Text.Encodings.Web;
@@ -12,11 +13,12 @@ public class ExecutionDetailsModel(JobExecutionService _jobExecutionService, Job
     public ExecutionViewModel Execution { get; set; }
     public JobViewModel? Job { get; set; }
     public string JobOutputProcessed { get; set; } = string.Empty;
+    public int JobOutputProcessedLines { get; set; }
 
-    public void OnGet(Guid id)
+    public void OnGet(Guid id, [FromQuery] int? lastLineCount)
     {
         Execution = _jobExecutionService.GetExecution(id);
-        JobOutputProcessed = ProcessJobOutput(_jobExecutionService.GetOutputForExecution(id));
+        JobOutputProcessed = ProcessJobOutput(_jobExecutionService.GetOutputForExecution(id), lastLineCount ?? 0);
 
         try
         {
@@ -28,10 +30,12 @@ public class ExecutionDetailsModel(JobExecutionService _jobExecutionService, Job
         }
     }
 
-    private string ProcessJobOutput(string output)
+    private string ProcessJobOutput(string output, int lastLineCount)
     {
         var stb = new StringBuilder();
-        foreach(var line in output.Split('\n'))
+        var splitLines = output.TrimEnd('\r', '\n').Split('\n');
+        JobOutputProcessedLines = splitLines.Length; // at least for now, the number of raw lines is the same as the number of processed lines
+        foreach(var line in splitLines.Skip(lastLineCount))
         {
             var l = line.Trim();
             if (l.Length >= 18 && l[1] == ' ' && l[10] == ' ' && l[17] == ' ')
@@ -40,13 +44,13 @@ public class ExecutionDetailsModel(JobExecutionService _jobExecutionService, Job
                 var spanClass = (l[0] == 'E' ? "stderr" : "stdout");
                 if (DateTimeOffset.TryParseExact(l.AsSpan(2, 15), "yyyyMMdd HHmmss", null, System.Globalization.DateTimeStyles.AssumeUniversal, out var timestamp))
                 {
-                    stb.AppendLine($"<span class=\"{spanClass}\"><span class=\"unselectable me-2\">{timestamp.ToLocalTime():yyyy-MM-dd HH:mm:ss}</span>{l.AsSpan(18..)}</span>");
+                    stb.Append($"<span class=\"{spanClass}\"><span class=\"unselectable timestamp me-2\">{timestamp.ToLocalTime():yyyy-MM-dd HH:mm:ss}</span>{l.AsSpan(18..)}\r\n</span>");
                 }
             }
             else
             {
                 // Append this line as is, because something is odd about it
-                stb.AppendLine($"<span>{_htmlEncoder.Encode(l)}</span>");
+                stb.Append($"<span>{_htmlEncoder.Encode(l)}\r\n</span>");
             }
         }
         return stb.ToString();
