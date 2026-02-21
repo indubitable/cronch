fe1fb17ce0603daf188b446da8d73e8524b70d46
Sasha Kotlyar <arktronic@users.noreply.github.com>
2026-02-21 09:51:33 -0500

    Improve robustness of config file handling

--- cronch/Services/ConfigPersistenceService.cs
+++ cronch/Services/ConfigPersistenceService.cs
@@ -10,57 +10,63 @@ public class ConfigPersistenceService(ILogger<ConfigPersistenceService> _logger,
 
     public ConfigPersistenceModel? Load()
     {
-        var location = _configuration["ConfigLocation"];
-        if (location == null)
+        lock (_serializer)
         {
-            _logger.LogError("Cannot load configuration: ConfigLocation is not set");
-            throw new InvalidOperationException();
-        }
+            var location = _configuration["ConfigLocation"];
+            if (location == null)
+            {
+                _logger.LogError("Cannot load configuration: ConfigLocation is not set");
+                throw new InvalidOperationException();
+            }
 
-        var filePathname = Path.Combine(location, CONFIG_FILE);
+            var filePathname = Path.Combine(location, CONFIG_FILE);
 
-        if (!File.Exists(filePathname))
-        {
-            _logger.LogDebug("Cannot load configuration: no such file exists");
-            return null;
-        }
+            if (!File.Exists(filePathname))
+            {
+                _logger.LogDebug("Cannot load configuration: no such file exists");
+                return null;
+            }
 
-        try
-        {
-            using var fileStream = File.OpenRead(filePathname);
-            var config = _serializer.Deserialize(fileStream) as ConfigPersistenceModel;
-            return config;
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Cannot load configuration: unexpected error");
-            throw;
+            try
+            {
+                using var fileStream = File.OpenRead(filePathname);
+                var config = _serializer.Deserialize(fileStream) as ConfigPersistenceModel;
+                return config;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Cannot load configuration: unexpected error");
+                throw;
+            }
         }
     }
 
     public void Save(ConfigPersistenceModel model)
     {
-        var location = _configuration["ConfigLocation"];
-        if (location == null)
+        lock (_serializer)
         {
-            _logger.LogError("Cannot save configuration: ConfigLocation is not set");
-            throw new InvalidOperationException();
-        }
+            var location = _configuration["ConfigLocation"];
+            if (location == null)
+            {
+                _logger.LogError("Cannot save configuration: ConfigLocation is not set");
+                throw new InvalidOperationException();
+            }
 
-        ArrangeModel(model);
+            ArrangeModel(model);
 
-        Directory.CreateDirectory(location);
-        var filePathname = Path.Combine(location, CONFIG_FILE);
+            Directory.CreateDirectory(location);
+            var filePathname = Path.Combine(location, CONFIG_FILE);
 
-        try
-        {
-            using var fileStream = File.Open(filePathname, FileMode.Create, FileAccess.Write, FileShare.Read);
-            _serializer.Serialize(fileStream, model);
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Cannot save configuration: unexpected error");
-            throw;
+            try
+            {
+                using var fileStream = File.Open(filePathname, FileMode.Create, FileAccess.Write, FileShare.Read);
+                _serializer.Serialize(fileStream, model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Cannot save configuration: unexpected error");
+                throw;
+            }
         }
     }
 
